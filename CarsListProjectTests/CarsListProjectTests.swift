239089ac2a0a83d02fb73ff26a97d800c9ea07d6
MGOLLI <mgolli@mbp-mczyz.home>
2018-07-09 21:57:57 +0200

    Initial Commit

---
+++ CarsListProjectTests/CarsListProjectTests.swift
@@ -0,0 +1,36 @@
+//
+//  CarsListProjectTests.swift
+//  CarsListProjectTests
+//
+//  Created by MGOLLI on 09.07.2018.
+//  Copyright © 2018 MGOLLI. All rights reserved.
+//
+
+import XCTest
+@testable import CarsListProject
+
+class CarsListProjectTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
